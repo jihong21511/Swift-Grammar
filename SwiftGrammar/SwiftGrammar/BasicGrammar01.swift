f12d3e87279d4eeac98bc073739c0ea3a5d75c93
jihong21511 <jihong21511@naver.com>
2020-08-07 13:13:03 +0900

    Constant,Variable,ConsoleLog

---
+++ SwiftGrammar/SwiftGrammar/BasicGrammar01.swift
@@ -0,0 +1,41 @@
+//
+//  BasicGrammar.swift
+//  SwiftGrammar
+//
+//  Created by JIHONG on 2020/08/06.
+//  Copyright © 2020 JIHONG. All rights reserved.
+//
+
+import Foundation
+ let age: Int = 10
+class Person {
+    var name: String = "Hongz"
+    var age: Int = 10
+
+}
+func basicGramme(){
+   //1. 이름짓기
+      //Lower Canel Case : fuction, method, variable, constant
+      //ex) someVariableName
+
+      //Upper Camel Case : type(class, struct, enum. extension)
+      //ex) Person, Point, Week
+
+      //대소문지를 구분
+   
+      //2. 콘솔로그
+    "안녕하세요! 홍즈의 나이는 \(age + 16)입니다."
+    
+    print("안녕하세요! 홍즈의 나이는 \(age + 16)입니다.")
+    
+}
+
+
+let Hongz: Person = Person()
+
+func printf(){
+    print(Hongz)
+    
+    dump(Hongz)
+}
+
---
+++ SwiftGrammar/SwiftGrammar/BasicGrammar02.swift
@@ -0,0 +1,31 @@
+//
+//  File.swift
+//  SwiftGrammar
+//
+//  Created by JIHONG on 2020/08/07.
+//  Copyright © 2020 JIHONG. All rights reserved.
+//
+
+import Foundation
+
+
+func basicGramme02(){
+    //상수와 변수
+     
+    //상수
+    //let 상수: 타입 = 값
+    let sum: Int
+    let inputA: Int = 100
+    let inputB: Int = 200
+     
+    //변수
+    //var 상수: 타입 = 값
+    var sum2: Int
+    
+    //선언 후 할당
+    sum = inputA + inputB
+    // sum = 1 바꾸려고 하면 error
+    
+    
+}
+
