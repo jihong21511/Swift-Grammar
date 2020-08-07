a64a4ed3ae542f6d531a9567fbead05ec8d2c9c2
jihong21511 <jihong21511@naver.com>
2020-08-07 15:18:30 +0900

    Basic Data Type

---
+++ SwiftGrammar/SwiftGrammar/BasicGrammar03.swift
@@ -0,0 +1,29 @@
+//
+//  BasicGrmaar03.swift
+//  SwiftGrammar
+//
+//  Created by JIHONG on 2020/08/07.
+//  Copyright © 2020 JIHONG. All rights reserved.
+//
+
+import Foundation
+
+//기본 데이터 타입
+
+// Int
+var someInt: Int = 100
+
+//UInt
+var someUInt: UInt = 100
+
+//Float
+var someFloat: Float = 3.14
+
+//Double
+var someDouble: Double = 3.14
+
+//Character(한글자, 이모티콘도 가능)
+var someCharcter: Character = "가"
+
+//String
+var someString: String = "가나다라마바사"
