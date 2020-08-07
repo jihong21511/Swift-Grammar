8c24de5ba254d70a1800f1f75688bec12321d65a
jihong21511 <jihong21511@naver.com>
2020-08-07 15:46:17 +0900

    Any, AnyObject, nil

---
+++ SwiftGrammar/SwiftGrammar/BasicGrammar04.swift
@@ -0,0 +1,22 @@
+//
+//  BasicGrammar04.swift
+//  SwiftGrammar
+//
+//  Created by JIHONG on 2020/08/07.
+//  Copyright © 2020 JIHONG. All rights reserved.
+//
+
+import Foundation
+
+// Any,AnyObect, nill
+
+//Any
+var something: Any = 100.4
+
+//AnyObect
+class someClass {}
+
+var someObect: AnyObject = someClass()
+
+//nil
+//nil = null
